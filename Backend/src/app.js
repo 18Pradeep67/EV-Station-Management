7121bad868fa7fae8ca1d170fb7f3a3550b015b2
18Pradeep67 <sritejapradeep1867@gmail.com>
2025-05-31 17:19:34 +0530

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/database.js';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+connectDB();
+
+app.listen(PORT,()=>{
+    console.log(`Server boom boom on ${PORT}`);
+});
