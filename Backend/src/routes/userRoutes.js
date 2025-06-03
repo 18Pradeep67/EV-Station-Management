e1734ba2e1c1f3c31d715c1bf58bf11758d57896
18Pradeep67 <sritejapradeep1867@gmail.com>
2025-06-03 18:18:54 +0530

    health check

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/health",health);
+router.get("/health",health);
 
 export default router;
